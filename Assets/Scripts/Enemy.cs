6d3c7ff1219364cb46e230690c4d5d459169d129
dmitrycdz <dmitrycdz@gmail.com>
2017-11-12 23:51:12 +0800

    20171112

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10;
+    public float hp = 150;
+    private float totalHp;
+    public GameObject explosionEffect;
+    private Slider hpSlider;
+
+    public int damage = 50;
+    public int enemyMoney = 100;
+    private Player player;
+    public bool isAirunit = false;
+    public NavMeshAgent agent;
+    // Use this for initialization
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+	void Start () {
+        totalHp = hp;
+        hpSlider = GetComponentInChildren<Slider>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed * player.perk.mobspeed_adj;
+        agent.destination = GameObject.Find("End").GetComponent<Transform>().position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+
+    void Move()
+    {
+        agent.destination = GameObject.Find("End").GetComponent<Transform>().position;
+        /*if (index > positions.Length - 1) return;
+        transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * speed);
+        if (Vector3.Distance(positions[index].position, transform.position) < 0.2f)
+        {
+            index++;
+        }*/
+        if (!agent.pathPending && agent.remainingDistance != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            ReachDestination();
+        }
+    }
+    //达到终点
+    void ReachDestination()
+    {
+        player.Playerhp -= damage;
+        GameObject.Destroy(this.gameObject);
+    }
+
+
+    void OnDestroy()
+    {
+        EnemySpawner.CountEnemyAlive--;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (hp <= 0) return;
+        hp -= damage;
+        hpSlider.value = (float)hp / totalHp;
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        GameObject effect = GameObject.Instantiate(explosionEffect, transform.position, transform.rotation);
+        player.ChangeMoney((int)(enemyMoney*player.perk.money_adj));
+        Destroy(effect, 1.5f);
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/MapCube.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MapCube : MonoBehaviour {
+    [HideInInspector]
+    public GameObject turretGo;//保存当前cube身上的炮台
+    [HideInInspector]
+    public TurretData turretData;
+    [HideInInspector]
+    public bool isUpgraded = false;
+
+    public GameObject buildEffect;
+    private Player player;
+    private Renderer renderer;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    public void BuildTurret(TurretData turretData)
+    {
+        this.turretData = turretData;
+        isUpgraded = false;
+        turretGo = GameObject.Instantiate(turretData.turretPrefab, transform.position, Quaternion.identity);
+        GameObject effect = GameObject.Instantiate(buildEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1.5f);
+    }
+
+    public void UpgradeTurret()
+    {
+        if(isUpgraded==true)return;
+
+        Destroy(turretGo);
+        isUpgraded = true;
+        turretGo = GameObject.Instantiate(turretData.turretUpgradedPrefab, transform.position, Quaternion.identity);
+        GameObject effect = GameObject.Instantiate(buildEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1.5f);
+    }
+    
+    public void DestroyTurret()
+    {
+        if (isUpgraded)
+        {
+            player.ChangeMoney((turretData.cost + turretData.costUpgraded) / 2);
+        }
+        else
+        {
+            player.ChangeMoney(turretData.cost/2);
+        }
+        Destroy(turretGo);
+        isUpgraded = false;
+        turretGo = null;
+        turretData=null;
+        GameObject effect = GameObject.Instantiate(buildEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1.5f);
+    }
+
+    void OnMouseEnter()
+    {
+
+        if (turretGo == null && EventSystem.current.IsPointerOverGameObject()==false)
+        {
+            renderer.material.color = Color.red;
+        }
+    }
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.white;
+    }
+}
